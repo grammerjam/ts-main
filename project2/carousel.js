818a0bc4cc2c20b50084b7c5a369406acfbc76c4
AdamPC <adam.s.blaisdell@gmail.com>
2023-05-10 22:32:06 -0400

    Added carousel and js page

---
+++ project2/carousel.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const slideContainer = document.querySelector('.carousel-container');
+  const prevButton = document.querySelector('.carousel-control.prev');
+  const nextButton = document.querySelector('.carousel-control.next');
+  const slideWidth = slideContainer.clientWidth;
+  const slideCount = slideContainer.children[0].childElementCount;
+  const slidesPerPage = 3;
+  let currentIndex = 0;
+
+  prevButton.addEventListener('click', function() {
+    if (currentIndex > 0) {
+      currentIndex--;
+    } 
+    slideContainer.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+  });
+
+  nextButton.addEventListener('click', function() {
+    console.log(currentIndex)
+    if (currentIndex <= Math.ceil(slideCount / slidesPerPage) - 2) {
+      currentIndex++;
+    } else {
+      currentIndex = 0;
+    }
+    slideContainer.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+  });
+});
