9434e78bb976261e2dd03d3a54430db372712269
AdamPC <adam.s.blaisdell@gmail.com>
2023-05-22 23:58:00 -0400

    Added validation card information

---
+++ project3/completed.js
@@ -0,0 +1,22 @@
+// get card image text elements
+var cardImageNumber = document.getElementById('cardImageNumber');
+var cardImageHolder = document.getElementById('cardImageHolder');
+var cardImageMonth = document.getElementById('cardImageMonth');
+
+
+window.onload = (event) => {
+    var inputCardNumber = sessionStorage.getItem('cardNumber');
+    cardImageNumber.innerHTML = inputCardNumber;
+
+    var inputCardHolder = sessionStorage.getItem('cardHolder');
+    cardImageHolder.innerHTML = inputCardHolder;
+
+    var inputMonth = sessionStorage.getItem('inputMonth');
+    cardImageMonth.innerHTML = inputMonth + '/';
+
+    var inputYear = sessionStorage.getItem('inputYear');
+    cardImageYear.innerHTML = inputYear;
+
+    var inputCvc = sessionStorage.getItem('inputCvc');
+    cardImageCvc.innerHTML = inputCvc;
+  };
