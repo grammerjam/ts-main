d88b5f95eeb7cf4da0fee53e9f7c8e977878792c
AdamPC <adam.s.blaisdell@gmail.com>
2023-06-15 20:11:58 -0400

    updated form to update card

--- project4/src/components/Card.jsx
+++ project4/src/components/Card.jsx
@@ -4,6 +4,7 @@ import BackCard from '../assets/images/bg-card-back.png'
 import Visa from '../assets/images/visa-logo.png'
 import Amex from '../assets/images/amex-logo.png'
 import { useCardContext } from '../context/CardContext'
+import { cardCvc, cardMonth, cardName, cardNumber, cardYear } from './Form'
 
 function Card(props){
 
@@ -14,18 +15,18 @@ function Card(props){
         <div class = {styles.cardContainer}>
             <img class = {styles.cardFront} src = {FrontCard}/>
             <div class = {styles.cardText}> 
-                <p class = {styles.cardImageNumber}>{props.infomation ? props.infomation : "0000 0000 0000 0000"}</p>
-                <p class = {styles.cardImageName}>{ items ? items :"Jane Appleseed"}</p>
+                <p class = {styles.cardImageNumber}>{cardNumber}</p>
+                <p class = {styles.cardImageName}>{cardName}</p>
                 <div class = {styles.cardImageDate}>
-                    <p class = {styles.cardImageMonth}>00/</p>
-                    <p class = {styles.cardImageYear}>00</p>
+                    <p class = {styles.cardImageMonth}>{cardMonth}/</p>
+                    <p class = {styles.cardImageYear}>{cardYear}</p>
                 </div>
                 <span class = {styles.bigCircle}></span>
                 <span class = {styles.smallCircle}></span>
                 <img class= {styles.cardImageCompany} src={Visa}/>
             </div>
             <img class = {styles.cardBack} src = {BackCard}/>
-            <p class = {styles.cardImageCvc}>000</p>
+            <p class = {styles.cardImageCvc}>{cardCvc}</p>
         </div>
         </>
         )
--- project4/src/components/Form.jsx
+++ project4/src/components/Form.jsx
@@ -1,7 +1,14 @@
 import styles from './Form.module.css'
-import Inputs from "../components/Inputs"
+// import Inputs from "../components/Inputs"
 import { A } from '@solidjs/router'
 import Card from './Card'
+import { createSignal } from "solid-js";
+
+export const [cardName, setCardName] = createSignal("Jane Appleseed");
+export const [cardNumber, setCardNumber] = createSignal("0000 0000 0000 0000");
+export const [cardMonth, setCardMonth] = createSignal("00");
+export const [cardYear, setCardYear] = createSignal("00");
+export const [cardCvc, setCardCvc] = createSignal("000");
 
 function Form() {
 
@@ -10,23 +17,28 @@ function Form() {
             <div className="col-md-8 px-5">
 
                 <form action="" class="needs-validation " novalidate>
-
+                <label class="form-label ms-5"> CARDHOLDER NAME </label>
                     {/* Card holder Name */}
-                    <Inputs id="inputCardHolder"
-                        name="CARDHOLDER NAME"
+                    <input id="inputCardHolder"
+                        class="form-control w-75 ms-5"
                         maxLength="40"
                         placeholder="e.g Jane Appleseed"
-                        error="Please input first and last name"
                         pattern="^([a-zA-Z]{1,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{1,}\s?([a-zA-Z]{1,})?)"
+                        onInput={(event) => setCardName( event.target.value )}
                     />
+                    <div class="invalid-feedback ms-5">
+                    "Please input first and last name"
+                    </div>
 
                     {/* Card number */}
-                    <Inputs id="inputCardNumber"
-                        name="CARD NUMBER"
+                    <label class="form-label ms-5"> CARD NUMBER </label>
+                    <input id="inputCardNumber"
+                        class="form-control w-75 ms-5"
                         maxLength="19"
                         placeholder="e.g 1234 5678 9123 0000"
                         error="Please input a valid card number"
                         pattern="^[0-9\s]{13,19}$"
+                        onInput={(event) => setCardNumber( event.target.value )}
                     />
 
                     {/* Section for 3 buttons */}
@@ -34,35 +46,41 @@ function Form() {
                         <div className="row row-cols-1">
 
                             {/* month */}
+                            <label class="form-label ms-5"> EXP.DATE </label>
                             <div className="col-3 ">
-                                <Inputs id="inputMonth"
-                                    name="EXP.DATE"
+                                <input id="inputMonth"
+                                    class="form-control w-75 ms-5"
                                     maxLength="2"
                                     placeholder="MM"
                                     error="Invalid month"
                                     pattern="^0[1-9]|1[0-2]$"
+                                    onInput={(event) => setCardMonth( event.target.value )}
                                 />
                             </div>
 
                             {/* Year */}
+                            <label class="form-label ms-5"> (MM/YY) </label>
                             <div className="col-3">
-                                <Inputs id="inputYear"
-                                    name="(MM/YY)"
+                                <input id="inputYear"
+                                    class="form-control w-75 ms-5"
                                     maxLength="2"
                                     placeholder="YY"
                                     error="Invalid year"
                                     pattern="^[2-9][3-9]$"
+                                    onInput={(event) => setCardYear( event.target.value )}
                                 />
                             </div>
 
                             {/* CVC */}
+                            <label class="form-label ms-5"> CVC </label>
                             <div className="col-4">
-                                <Inputs id="inputCvc"
-                                    name="CVC"
+                                <input id="inputCvc"
+                                    class="form-control w-75 ms-5"
                                     maxLength="3"
                                     placeholder="e.g 123"
                                     error="Invalid CVC"
                                     pattern="^[0-9][0-9][0-9]$"
+                                    onInput={(event) => setCardCvc( event.target.value )}
                                 />
                             </div>
 
